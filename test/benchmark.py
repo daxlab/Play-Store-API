443def677176dd3097c99eddf51c7f281677bdd2
Mandeep Singh <mandeep25894@gmail.com>
2016-03-19 22:09:09 +0530

    add time test

--- test/benchmark.py
+++ test/benchmark.py
@@ -1,11 +1,14 @@
 import urllib2
 import json
+import time
 
 url = "http://localhost:3000/api?package=com.whatsapp"
-
-for i in range(5):
+start_time = time.time()
+for i in range(1000):
     print 'Downloading '+ str(i)
     res = urllib2.urlopen(url).read()
     file = "data-"+str(i)+".json"
     with open(file, 'w') as outfile:
         json.dump(res, outfile)
+
+print("Time of execution %s seconds ..." % (time.time() - start_time))
